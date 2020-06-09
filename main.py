aeca97205b3d42c2fab77e3c95f0b5fcbc191655
helenjkifer <helenjkifer@gmail.com>
2020-06-09 16:32:09 +0000

    prime checker

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("hi")
-print("hello")
-print("helen")
+#
+#count = 0
+#for x in range(0, 1000):
+#    if x**2 % 7 == 2:
+#        count = count + 1
+#        print(x)
+#print("count is", count)
+
+import math
+
+def prime_checker(num):
+    upper = round(math.sqrt(num))+1
+    isprime = True
+    for x in range(2, upper):
+        #do stuff
+        if num % x == 0:
+            isprime = False
+            print(x)
+    print("is prime:", isprime)
+
+prime_checker(101)
